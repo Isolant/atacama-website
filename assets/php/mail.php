11e93033b92806e30798e49bcfe27cb60afb31cc
Juan Martín García <hello@juangarcia.com.ar>
2020-11-05 11:47:21 -0300

    Test with a different email

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -4,8 +4,8 @@
   $mailgun = Mailgun::create('pubkey-f9b47f60b5e823fe809bc0df1d64e505');
 
   $mailgun->messages()->send('mg.atacama.isolant.com.ar', [
-    'from'    => 'isolant@isolant.com.ar',
-    'to'      => 'hello@juangarcia.com.ar',
+    'from'    => 'isolant@mg.atacama.isolant.com.ar',
+    'to'      => 'juanmartingarcia1991@gmail.com',
     'subject' => 'The PHP SDK is awesome!',
     'text'    => 'It is so simple to send a message.'
   ]);
