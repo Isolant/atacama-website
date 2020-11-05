a8402a57a7d45eaeab8f8fa4d0c90b3e21cddc3c
Juan Martín García <hello@juangarcia.com.ar>
2020-11-05 13:25:19 -0300

    Fix on email sending

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,17 +1,46 @@
 <?php
+  // Require dependencies
   require '../../vendor/autoload.php';
   use Mailgun\Mailgun;
-  $mg = Mailgun::create('pubkey-f9b47f60b5e823fe809bc0df1d64e505');
 
+  // Get the data from the form
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $location = $_POST['location'];
+  $message = $_POST['message'];
+  
+  // Create the html structure
+  $htmlContent = "
+    Informaci&oacute;n de contacto:
+    <br /><br />
+    Nombre: ".$name."
+    <br />
+    Email: ".$email."
+    <br />
+    Telefono: ".$phone."
+    <br />
+    Provincia: ".$location."
+    <br />
+    Consulta: ".$message."
+  ";
+
+  // Create MG client
+  $mg = Mailgun::create('c75442aa270507406a690e15dddf33fa-0e6e8cad-8c68a9f4');
+
+  // Configure the email
   $domain = 'mg.isolant.com.ar';
   $params = array(
-    'from'    => 'Isolant Aislantes <isolant@isolant.com.ar>',
-    'to'      => 'hello@juangarcia.com.ar',
-    'subject' => 'Hello',
-    'text'    => 'Testing some Mailgun awesomness!'
+    'from'    => 'Landing Atacama <isolant@isolant.com.ar>',
+    'to'      => 'marketing@isolant.com.ar',
+    "h:Reply-To" => "{$email}",
+    'subject' => 'Nuevo correo enviado desde website Atacama',
+    "html" => "{$htmlContent}"
   );
 
+  // Send the email
   $mg->messages()->send($domain, $params);
 
-  echo 'sent';
+  // Redirect the user
+  header('Location: /gracias.html');
 ?>
