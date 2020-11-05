2e49c982b63cd0707723e2f94146c84d4fb6ffbc
Juan Martín García <hello@juangarcia.com.ar>
2020-11-05 11:29:38 -0300

    Email sending with mailgun and composer

---
+++ assets/php/mail.php
@@ -0,0 +1,12 @@
+<?php
+  require '../../vendor/autoload.php';
+  use Mailgun\Mailgun;
+  $mailgun = Mailgun::create('pubkey-f9b47f60b5e823fe809bc0df1d64e505');
+
+  $mg->messages()->send('mg.isolant.com', [
+    'from'    => 'isolant@isolant.com.ar',
+    'to'      => 'hello@juangarcia.com.ar',
+    'subject' => 'The PHP SDK is awesome!',
+    'text'    => 'It is so simple to send a message.'
+  ]);
+?>
