c2f05c67eb5f6f5fa3512edb7775f7a72acce521
Juan Martín García <hello@juangarcia.com.ar>
2020-11-05 11:53:55 -0300

    Fix on mailgun api

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,12 +1,15 @@
 <?php
   require '../../vendor/autoload.php';
   use Mailgun\Mailgun;
-  $mailgun = Mailgun::create('pubkey-f9b47f60b5e823fe809bc0df1d64e505');
+  $mgClient = new Mailgun('pubkey-f9b47f60b5e823fe809bc0df1d64e505');
+  $domain = "mg.atacama.isolant.com.ar";
 
-  $mailgun->messages()->send('mg.atacama.isolant.com.ar', [
+  $mgClient->sendMessage($domain, array(
     'from'    => 'isolant@mg.atacama.isolant.com.ar',
     'to'      => 'juanmartingarcia1991@gmail.com',
     'subject' => 'The PHP SDK is awesome!',
     'text'    => 'It is so simple to send a message.'
-  ]);
+  ));
+
+  echo 'test';
 ?>
