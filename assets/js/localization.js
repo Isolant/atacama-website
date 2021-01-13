ed8bce236b68cc556ad27aa662dadf3cbc858c1d
Juan Martín García <hello@juangarcia.com.ar>
2021-01-13 18:06:13 -0300

    Revert changes for geolocation

--- assets/js/localization.js
+++ assets/js/localization.js
@@ -1,83 +1,93 @@
 // ---------------------------------------------------- //
 
 // Old code, using the inaccurate geolocation db API
-// const buyButton = document.querySelector('.btn a');
+const buyButton = document.querySelector('.btn a');
 
-// const adjustUI = (data) => {
-//   const cities = ['Almirante Brown', 'Avellaneda', 'Berazategui', 'Buenos Aires', 'Berisso', 'Brandsen', 'Campana', 'Cañuelas', 'Ensenada', 'Escobar', 'Esteban Echeverría', 'Exaltación de la Cruz', 'Ezeiza', 'Florencio Varela', 'General Las Heras', 'General Rodríguez', 'General San Martín', 'Hurlingham', 'Ituzaingó', 'José C.Paz', 'La Matanza', 'Lanús', 'La Plata', 'Lomas de Zamora', 'Luján', 'Marcos Paz', 'Malvinas Argentinas', 'Moreno', 'Merlo', 'Morón', 'Pilar', 'Presidente Perón', 'Quilmes', 'San Fernando', 'San Isidro', 'San Miguel', 'San Vicente', 'Tigre', 'Tres de Febrero', 'Vicente López', 'Zárate', 'El Talar', null];
+const adjustUI = (data) => {
+  const cities = ['Almirante Brown', 'Avellaneda', 'Berazategui', 'Buenos Aires', 'Berisso', 'Brandsen', 'Campana', 'Cañuelas', 'Ensenada', 'Escobar', 'Esteban Echeverría', 'Exaltación de la Cruz', 'Ezeiza', 'Florencio Varela', 'General Las Heras', 'General Rodríguez', 'General San Martín', 'Hurlingham', 'Ituzaingó', 'José C.Paz', 'La Matanza', 'Lanús', 'La Plata', 'Lomas de Zamora', 'Luján', 'Marcos Paz', 'Malvinas Argentinas', 'Moreno', 'Merlo', 'Morón', 'Pilar', 'Presidente Perón', 'Quilmes', 'San Fernando', 'San Isidro', 'San Miguel', 'San Vicente', 'Tigre', 'Tres de Febrero', 'Vicente López', 'Zárate', 'El Talar', null];
 
-//   // If the current location exist on the array, return the fn
-//   const result = cities.find(city => city === data.city || city === data.state);
+  // If the current location exist on the array, return the fn
+  const result = cities.find(city => city === data.city || city === data.state);
 
-//   if(result !== undefined) {
-//     return;
-//   } else {
-//     buyButton.classList.add('hidden');
-//   }
-// }
+  if(result !== undefined) {
+    return;
+  } else {
+    buyButton.classList.add('hidden');
+  }
+}
 
-// const fetchLocation = async () => {
-//   // Get the data from the API
-//   const response = await fetch('https://geolocation-db.com/json/0f761a30-fe14-11e9-b59f-e53803842572/');
-//   const data = await response.json();
+const fetchLocation = async () => {
+  // Get the data from the API
+  const response = await fetch('https://geolocation-db.com/json/0f761a30-fe14-11e9-b59f-e53803842572/');
+  const data = await response.json();
 
-//   // Then adjust the elements we show on the UI
-//   adjustUI(data);
-// };
+  // Then adjust the elements we show on the UI
+  adjustUI(data);
+};
 
-// fetchLocation();
+fetchLocation();
 
 // ---------------------------------------------------- //
 
 // New code, using the HTML 5 geolocation feature
+// const buyButton = document.querySelector('.btn a');
 
-// We check if the geolocation functionality is supported by the browser
-const getLocation = () => {
-  if(navigator.geolocation) {
-    console.log('test');
-    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
-}
+// // We check if the geolocation functionality is supported by the browser
+// const getLocation = () => {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+//   } else {
+//     console.log("Geolocation is not supported by this browser.");
+//   }
+// }
 
-// Function to get the coordinates for the user
-const geoSuccess = (position) => {
-  const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("lat:" + lat + " lng:" + lng);
-  codeLatLng(lat, lng);
-}
+// // Function to get the coordinates for the user
+// const geoSuccess = (position) => {
+//   const lat = position.coords.latitude;
+//   const lng = position.coords.longitude;
+//   codeLatLng(lat, lng);
+// }
 
-// Error handling
-const geoError = () => {
-  alert("Geocoder failed.");
-}
+// // Error handling
+// const geoError = () => {
+//   console.log("Geocoder failed.");
+// }
 
-// Initialize geocoder
-let geocoder;
-const initialize = () => {
-  geocoder = new google.maps.Geocoder();
-}
+// // Initialize geocoder and get the location
+// let geocoder;
+// const initialize = () => {
+//   geocoder = new google.maps.Geocoder();
+//   getLocation();
+// }
 
-// Transform the lat and lng into a comprehensive city
-const codeLatLng = (lat, lng) => {
-  const latlng = new google.maps.LatLng(lat, lng);
+// // Transform the lat and lng into a comprehensive city
+// const codeLatLng = (lat, lng) => {
+//   const latlng = new google.maps.LatLng(lat, lng);
 
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if(status == google.maps.GeocoderStatus.OK) {
-      console.log(results)
-      if(results[1]) {
-        // Formatted address
-        const address = results[0].formatted_address;
-        alert("address = " + address);
-      } else {
-        alert("No results found");
-      }
-    } else {
-      alert("Geocoder failed due to: " + status);
-    }
-  });
-}
+//   geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       if (results[1]) {
+//         // Get the city
+//         const location = results[0].address_components[4].long_name;
 
-initialize();
+//         // Array to hold the cities
+//         const cities = ['Almirante Brown', 'Avellaneda', 'Berazategui', 'Buenos Aires', 'Berisso', 'Brandsen', 'Campana', 'Cañuelas', 'Ensenada', 'Escobar', 'Esteban Echeverría', 'Exaltación de la Cruz', 'Ezeiza', 'Florencio Varela', 'General Las Heras', 'General Rodríguez', 'General San Martín', 'Hurlingham', 'Ituzaingó', 'José C.Paz', 'La Matanza', 'Lanús', 'La Plata', 'Lomas de Zamora', 'Luján', 'Marcos Paz', 'Malvinas Argentinas', 'Moreno', 'Merlo', 'Morón', 'Pilar', 'Presidente Perón', 'Quilmes', 'San Fernando', 'San Isidro', 'San Miguel', 'San Vicente', 'Tigre', 'Tres de Febrero', 'Vicente López', 'Zárate', 'El Talar'];
+
+//         // If the current location exist on the array, return the fn
+//         const result = cities.find(city => city === location);
+
+//         if (result !== undefined) {
+//           return;
+//         } else {
+//           buyButton.classList.add('hidden');
+//         }
+//       } else {
+//         console.log("No results found");
+//       }
+//     } else {
+//       console.log("Geocoder failed due to: " + status);
+//     }
+//   });
+// }
+
+// initialize();
